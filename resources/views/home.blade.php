6db2cb4cc1e1dae8c682e2a6b8ebb0d04ab76c9d
P4anix <pancok_1@yahoo.it>
2023-04-30 13:07:37 +0200

    primi passi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primo esercizio con Laravel</title>
+</head>
+
+<body>
+    <h1>HELLO WORLD!</h1>
+    <p>{{$title}}</p>
+    <p>{{$text}}</p>
+    <p>Gli alunni della Classe {{$num}}</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+     $data = [
+        'title' => 'Magari fosse cosi facile!!',
+        'text' => 'Saremmo tutti dei gran PRO',
+        'num' => 89
+    ];
+
+    return view('home', $data);
 });
